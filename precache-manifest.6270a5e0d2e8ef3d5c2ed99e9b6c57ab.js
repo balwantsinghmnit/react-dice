afb5b7960f52050d6ed73f811b44c6af3390b778
balwantsinghmnit <balwantsinghmnit@gmail.com>
2020-07-19 12:09:36 +0530

    Updates

---
+++ precache-manifest.6270a5e0d2e8ef3d5c2ed99e9b6c57ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcab496b1845adbfa173e06b2c0311ae",
+    "url": "/react-dice/index.html"
+  },
+  {
+    "revision": "29a66b6b057d50075c18",
+    "url": "/react-dice/static/css/main.aedb1095.chunk.css"
+  },
+  {
+    "revision": "3bf8bc923fb905838459",
+    "url": "/react-dice/static/js/2.fe1fb92b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-dice/static/js/2.fe1fb92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a66b6b057d50075c18",
+    "url": "/react-dice/static/js/main.5cf92f8e.chunk.js"
+  },
+  {
+    "revision": "ae6ac300d77053048312",
+    "url": "/react-dice/static/js/runtime-main.e228eefd.js"
+  }
+]);
